bed4c7b4949332b86c798e346206ecc462cf103f
Kalar2025 <kalarsu@gmail.com>
2025-04-05 00:41:49 -0700

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
+import Link from "next/link";
+import Head from "next/head";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,6 +23,11 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: Readonly<{children: React.ReactNode;}>) {
   return (
     <html lang="en" suppressHydrationWarning>
+    <Head>
+        <title>Chi's Portfolio</title>
+        <Link rel="icon" href="/favicon.ico" />
+    </Head>
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-auto`}
       >
